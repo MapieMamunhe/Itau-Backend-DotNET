326167e1be7be01cd718b49763d035ee762d8b50
Mapie Mamunhe <mmamunhe@gmail.com>
2025-12-14 23:30:04 +0200

    Add loggin configuration

--- Program.cs
+++ Program.cs
@@ -1,35 +1,67 @@
-using Microsoft.OpenApi;
+﻿using Microsoft.OpenApi;
+using Serilog;
+using Serilog.Events;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+var logPath = Path.Combine(builder.Environment.ContentRootPath, "Logs");
 
-builder.Services.AddSwaggerGen(options =>
+Directory.CreateDirectory(logPath); 
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .WriteTo.Console()
+    .WriteTo.File(
+        Path.Combine(logPath, "log-.txt"),
+        rollingInterval: RollingInterval.Day
+    )
+    .CreateLogger();
+
+try
 {
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "ITAU Backend", Version = "v1" });
-});
+    Log.Information("Starting web application");
+    builder.Services.AddSerilog();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-var app = builder.Build();
-app.MapGet("/", () => Results.Redirect("/swagger"));
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapSwagger();
-    app.UseSwaggerUI(options =>
+    builder.Services.AddSwaggerGen(options =>
     {
-        options.SwaggerEndpoint("v1/swagger.json", "ITAU Backend");
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "ITAU Backend", Version = "v1" });
     });
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    var app = builder.Build();
+    app.MapGet("/", () => Results.Redirect("/swagger"));
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("v1/swagger.json", "ITAU Backend");
+        });
+    }
+
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
