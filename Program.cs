d6b77040acf3c091cb66d7fbfff3f85c38d29caa
Mapie Mamunhe <mmamunhe@gmail.com>
2025-12-15 00:04:49 +0200

    Add metrics configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.OpenApi;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 using Serilog.Events;
 
@@ -24,6 +27,61 @@ Log.Logger = new LoggerConfiguration()
 try
 {
     Log.Information("Starting web application");
+    builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
+
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+                         "Microsoft.AspNetCore.Server.Kestrel");
+        builder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+                       0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+            });
+    });
+
+    // MEtricas
+    var tracingOtlpEndpoint = builder.Configuration["OTLP_ENDPOINT_URL"];
+    var otel = builder.Services.AddOpenTelemetry();
+
+    // Configure OpenTelemetry Resources with the application name
+    otel.ConfigureResource(resource => resource
+        .AddService(serviceName: builder.Environment.ApplicationName));
+
+    // Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+    otel.WithMetrics(metrics => metrics
+        // Metrics provider from OpenTelemetry
+        .AddAspNetCoreInstrumentation()
+        // Metrics provides by ASP.NET Core in .NET 8
+        .AddMeter("Microsoft.AspNetCore.Hosting")
+        .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+        // Metrics provided by System.Net libraries
+        .AddMeter("System.Net.Http")
+        .AddMeter("System.Net.NameResolution")
+        .AddPrometheusExporter());
+
+    // Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+    otel.WithTracing(tracing =>
+    {
+        tracing.AddAspNetCoreInstrumentation();
+        tracing.AddHttpClientInstrumentation();
+        if (tracingOtlpEndpoint != null)
+        {
+            tracing.AddOtlpExporter(otlpOptions =>
+            {
+                otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint);
+            });
+        }
+        else
+        {
+            tracing.AddConsoleExporter();
+        }
+    });
+
+    // Fim das metricas
+
     builder.Services.AddSerilog();
 
     builder.Services.AddSwaggerGen(options =>
@@ -54,6 +112,7 @@ try
     app.UseAuthorization();
 
     app.MapControllers();
+    app.MapPrometheusScrapingEndpoint();
 
     app.Run();
 }
