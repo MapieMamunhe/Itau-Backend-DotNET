d9808ef731b3377f57d5bcdaa2713e48cfc86496
Mapie Mamunhe <mmamunhe@gmail.com>
2025-12-14 22:37:58 +0200

    Add Swagger  to document my application endpoints

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "ITAU Backend", Version = "v1" });
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("v1/swagger.json", "ITAU Backend");
+    });
 }
+
 
 app.UseHttpsRedirection();
 
