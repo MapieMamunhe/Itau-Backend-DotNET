fdaa70f4f18d9137658de6b35a9cc8e6b3b04d75
Mapie Mamunhe <mmamunhe@gmail.com>
2025-12-15 22:01:34 +0200

    Add basic Transacao endpoint

---
+++ Controllers/TransacaoController.cs
@@ -0,0 +1,33 @@
+﻿using Itau_Backend.Models;
+using Itau_Backend.Models.DTO;
+using Itau_Backend.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Itau_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransacaoController : Controller
+    {
+        private readonly ILogger<TransacaoController> _logger;
+        private readonly ITransacaoRepository _transacaoRepository;
+        public TransacaoController(ILogger<TransacaoController> logger,
+            ITransacaoRepository transacaoRepository)
+        {
+            _logger = logger;
+            _transacaoRepository = transacaoRepository;
+        }
+        [HttpPost]
+        public IActionResult Post(TransacaoRequest transacao)
+        {
+            _transacaoRepository.Nova (transacao.ToEntity());
+            return Created();
+        }
+        [HttpGet]
+        public ActionResult<List<TransacaoResponse>> Get()
+        {
+            var transactions = _transacaoRepository.Listar().Select(item=>item.ToDto());
+            return Ok(transactions);
+        }
+    }
+}
