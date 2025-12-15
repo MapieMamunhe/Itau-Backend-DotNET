8c8a33015ab20d53ee4a74bf01f4cc22ea2e0e24
Mapie Mamunhe <mmamunhe@gmail.com>
2025-12-15 22:57:00 +0200

    Add Testing folder to solution

---
+++ Itau Testes/TransacaoRepositoryTest.cs
@@ -0,0 +1,43 @@
+﻿using Itau_Backend.Exceptions;
+using Itau_Backend.Models;
+using Itau_Backend.Repository;
+
+namespace Itau_Testes
+{
+    public class TransacaoRepositoryTest
+    {
+        private readonly ITransacaoRepository _transacaoRepository;
+
+        public TransacaoRepositoryTest()
+        {
+            _transacaoRepository = new TransacaoRepository();
+        }
+
+        [Fact]
+        
+        public void TransacaoShouldNotBeInTheFuture()
+        {
+            Transacao transacao = new Transacao
+            {
+                Id = 1,
+                Valor = 100.00m,
+                DataHora = DateTime.Now.AddDays(1) 
+            };
+            Assert
+                .Throws<DataTransacaoNoFuturoException>(()=>_transacaoRepository.Nova(transacao));
+        }
+        [Fact]
+        public void TransactionValueShouldNotBeLessThanZero()
+        {
+            Transacao transacao = new Transacao
+            {
+                Id = 1,
+                Valor = -1.0m,
+                DataHora = DateTime.UtcNow 
+            };
+            Assert
+                .Throws<ValorDaTransacaoMenorQueZero>(() => _transacaoRepository.Nova(transacao));
+
+        }
+    }
+}
